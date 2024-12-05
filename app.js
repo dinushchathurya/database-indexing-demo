3aae8a07a9c529f937d340c77514d50c3dd732e0
Dinush Chathurya <dinushchathurya21@gmail.com>
2024-12-05 21:36:41 +0000

    feat: Create basic application to show query time
    Signed-off-by: Dinush Chathurya <dinushchathurya21@gmail.com>

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
-// app.js
-const express = require("express");
-const bodyParser = require("body-parser");
-const userRoutes = require("./routes/userRoutes");
-const logger = require("./utils/logger");
+const express = require('express');
+const mysql = require('mysql2');
 
 const app = express();
 const port = 3000;
 
-// Middleware
-app.use(bodyParser.json());
+// MySQL connection pool
+const pool = mysql.createPool({
+    host: 'localhost',    // Change this to your MySQL server IP
+    user: 'root',         // MySQL user
+    password: '', // MySQL password
+    database: 'cluster_index_demo',     // Database name
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
-// Routes
-app.use("/api", userRoutes);
+// Fetch user by name (force no indexing, using full table scan)
+app.get('/user-no-index/:name', (req, res) => {
+    const { name } = req.params;
+    console.time('query-time-no-index');  // Start time tracking
+    pool.query('SELECT * FROM users FORCE INDEX (PRIMARY) WHERE name = ?', [name], (err, results) => {
+		console.timeEnd('query-time-no-index');  // Log the query time
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.json(results);
+		}
+    });
+});
 
-// Start the server
+// Fetch user by name (using index for optimized query)
+app.get('/user-with-index/:name', (req, res) => {
+    const { name } = req.params;
+    console.time('query-time-with-index');  // Start time tracking
+    pool.query('SELECT * FROM users WHERE name = ?', [name], (err, results) => {
+		console.timeEnd('query-time-with-index');  // Log the query time
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.json(results);
+		}
+    });
+});
+
 app.listen(port, () => {
-  logger.info(`Server running on http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userController");
-
-router.get("/index", userController.getUsersWithIndex);
-router.get("/noindex", userController.getUsersWithoutIndex);
-
-module.exports = router;
